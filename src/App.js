57c5d8b2cd3ac01c02a11309839608e19b9b3355
Tahmina <tahminakhatun5447@gmail.com>
2022-11-08 23:31:27 +0600

    add my review page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='max-w-screen-lg mx-auto'>
-        {/* add data */}
+        
         {/* add item */}
         {/* add review */}
       <RouterProvider  router={router}></RouterProvider>
