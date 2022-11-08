17c518d64e3aff78381a5887d5a1ad0b588538ca
Tahmina <tahminakhatun5447@gmail.com>
2022-11-08 23:33:17 +0600

    add blog page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <div className='max-w-screen-lg mx-auto'>
         {/* add data */}
-        {/* add item */}
+        
         {/* add review */}
       <RouterProvider  router={router}></RouterProvider>
     </div>
